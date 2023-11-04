3a62038fb7e77efe991c4ef67bb0b89e4976ff88
Anton <kucherenko-anton1@mail.ru>
2023-11-04 18:29:01 +0200

    reinstalled tailwind

--- final_assignment/src/App.tsx
+++ final_assignment/src/App.tsx
@@ -4,13 +4,13 @@ import Tasks from './views/Tasks';
 import About from './views/About';
 import Settings from './views/Settings';
 import Header from './components/Header/Header';
-import "./App.css";
+
 
 const App: React.FC = () => {
   return (
     <Router>
       <Header />
-      <div className="app-container">
+      <div className="mx-6 my-6">
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/tasks" element={<Tasks />} />
